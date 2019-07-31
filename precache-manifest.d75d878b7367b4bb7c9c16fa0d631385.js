d40b0ecd508600494a999a9b88ce01e5254fc636
Pontus Nilsson <pontus.nilsson@combitech.se>
2019-07-31 13:23:41 +0200

    Updates

---
+++ precache-manifest.d75d878b7367b4bb7c9c16fa0d631385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d391b80bc71d8e7c6bd3065c010680a5",
+    "url": "/SearchLyrics/index.html"
+  },
+  {
+    "revision": "7f10ada11c7601865e75",
+    "url": "/SearchLyrics/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cacc6293b764a6467f64",
+    "url": "/SearchLyrics/static/js/2.3b25e957.chunk.js"
+  },
+  {
+    "revision": "7f10ada11c7601865e75",
+    "url": "/SearchLyrics/static/js/main.f9d57de5.chunk.js"
+  },
+  {
+    "revision": "6347de781ced66c205c8",
+    "url": "/SearchLyrics/static/js/runtime~main.6771eaca.js"
+  },
+  {
+    "revision": "b95289c60f17b81d29ced93c8db12f3b",
+    "url": "/SearchLyrics/static/media/spinner.b95289c6.gif"
+  }
+]);
